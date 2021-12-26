4215c50b8554c7ca5c6d5f2fb914ed13e28e30bc
Petr Fifka <petrfifka@Petrs-MacBook-Pro.local>
2021-12-26 18:25:30 +0100

    Add folder create to testcases

--- src/main/java/core/web/WebFrameworkManager.java
+++ src/main/java/core/web/WebFrameworkManager.java
@@ -1,13 +1,18 @@
 package core.web;
 
+import core.data.shared.TestCaseData;
 import core.shared.ConfigManager;
 import core.shared.FrameworkManager;
 import core.shared.RunManager;
+
+import static core.shared.TestType.WEB;
 
 public class WebFrameworkManager implements FrameworkManager {
 
     @Override
     public void init() {
+        new RunManager().createRunFolder("WebTest");
+        TestCaseData.setTestType(WEB);
         ConfigManager.init();
         WebDriverManager.getWebDriverManager().init();
     }
