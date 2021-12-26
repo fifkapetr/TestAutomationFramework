00b15a2739960535256baab58c46dd5112bf64ab
Petr Fifka <petrfifka@Petrs-MacBook-Pro.local>
2021-12-26 17:50:17 +0100

    RunManager tests

--- src/test/java/TEMP.java
+++ src/test/java/TEMP.java
@@ -1,3 +1,4 @@
+import core.shared.RunManager;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pageobjects.unit.ParentXPathObject;
@@ -5,14 +6,20 @@ import unit.web.mock.MockTestCase;
 
 public class TEMP {
 
-    @BeforeTest
-    public void initMock() {
-        MockTestCase mockTestCase = new MockTestCase();
-        mockTestCase.init("PROD", "chrome");
-    }
+//    @BeforeTest
+//    public void initMock() {
+//        MockTestCase mockTestCase = new MockTestCase();
+//        mockTestCase.init("PROD", "chrome");
+//    }
+//
+//    @Test
+//    public void test(){
+//        new ParentXPathObject();
+//    }
 
     @Test
-    public void test(){
-        new ParentXPathObject();
+    public void test() {
+        RunManager runManager = new RunManager();
+        runManager.createRunFolder("MobileTest");
     }
 }
--- src/test/java/core/RunManagerTests.java
+++ src/test/java/core/RunManagerTests.java
@@ -1,2 +1,39 @@
-package core;public class RunManager {
+package core;
+
+import core.shared.RunManager;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class RunManagerTests {
+
+    String testType = "UnitTest";
+
+    @Test
+    public void generatedRunFolderNameNotNull() {
+        RunManager runManager = new RunManager();
+        String path = runManager.generateRunFolderName(testType);
+        Assert.assertNotNull(path);
+    }
+
+    @Test
+    public void getRunFolderPathNotNull() {
+        RunManager runManager = new RunManager();
+        runManager.createRunFolder(testType);
+        Assert.assertNotNull(RunManager.getRunFolderPath());
+    }
+
+    @Test
+    public void getRunFolderPathNotNullCreateNotCalled() {
+        Assert.assertNotNull(RunManager.getRunFolderPath());
+    }
+
+    @Test
+    public void generatedFolderExists() {
+        RunManager runManager = new RunManager();
+        runManager.createRunFolder(testType);
+        Assert.assertTrue(new File(RunManager.getRunFolderPath()).exists());
+    }
+
 }
