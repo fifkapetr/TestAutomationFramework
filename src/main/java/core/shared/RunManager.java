1f547d58da2d9872d971a39d7757f7bfbeedd3c8
Petr Fifka <petrfifka@Petrs-MacBook-Pro.local>
2021-12-27 12:35:52 +0100

    add utils call to get timestamp in RunManager

--- src/main/java/core/shared/RunManager.java
+++ src/main/java/core/shared/RunManager.java
@@ -2,23 +2,20 @@ package core.shared;
 
 import core.Log;
 import core.data.shared.TestCaseData;
+import util.Utils;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class RunManager {
 
     public String generateRunFolderName(String testType) {
-        Long time = new Date().getTime();
-        String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS").format(time);
-        String dirName = testType + " " + timestamp;
+        String dirName = testType + " " + Utils.getCurrentTimestampWithFormat("yyyy-MM-dd HH:mm:ss,SSS");
         Log.debug("Run folder name was generated: " + dirName);
         return dirName;
     }
 
     public void createRunFolder(String testType) {
-        TestCaseData.setRunFolderPath("target/test-results/" + generateRunFolderName(testType));
+        TestCaseData.setRunFolderPath("target/test-results/" + generateRunFolderName(testType) + "/");
         new File(TestCaseData.getRunFolderPath()).mkdirs();
     }
 
